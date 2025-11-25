a3ca461c3d50900bc292da8981ec66be646f4ade
jinyus <starboymc4@gmail.com>
2025-11-25 15:22:03 -0500

    tidy

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,9 @@ import 'package:system_theme/system_theme.dart';
 import 'package:yaru/widgets.dart';
 
 import 'app/mediadojo.dart';
-import 'register_dependencies.dart';
 
 Future<void> main() async {
   await YaruWindowTitleBar.ensureInitialized();
   await SystemTheme.accentColor.load();
-  await startUp();
   runApp(const MediaDojo());
 }
